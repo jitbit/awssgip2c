883cae8dcb84989afdb1cab6e41894d5f618cf43
jitbit <jitbit@gmail.com>
2017-05-12 22:09:49 +0300

    added console logging, also works with AWS new css

--- main.js
+++ main.js
@@ -2,13 +2,17 @@
 document
     .getElementsByName("source")
     .forEach(function(element) {
-        var url = "https://freegeoip.net/json/" + element.value.substring(0, element.value.indexOf('/'));
+        var ipAddress = element.value.substring(0, element.value.indexOf('/'));
+        var url = "https://freegeoip.net/json/" + ipAddress;
         var xmlHttp = new XMLHttpRequest();
         xmlHttp.onreadystatechange = function() { 
             if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
                 var location = JSON.parse(xmlHttp.responseText)
                 var cn = location.country_code + ", " + location.region_name;
+                console.log(ipAddress + " - " + cn);
                 var span = document.createElement("span");
+                span.style.position = "absolute";
+                span.style.right = "0px";
                 span.innerText = cn;
                 element.parentElement.appendChild(span);
             }
